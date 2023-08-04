f2d7b256d50b5f6ba2de8cb125f9fb0c5c3abf6b
alexandreakdeniz <alexandre.akdeniz@free.fr>
2023-08-05 00:01:49 +1200

    Remove a useless commentary

--- FappCommon/FappCommon/MongoDbContext/BaseMongoTest.cs
+++ FappCommon/FappCommon/MongoDbContext/BaseMongoTest.cs
@@ -4,14 +4,13 @@ public abstract class BaseMongoTest<TMockMongoContext>/* : IDisposable*/
     where TMockMongoContext : BaseMockMongoDbContext, new()
 {
     protected readonly BaseMongoDatabaseFixture<TMockMongoContext> Fixture;
-    protected readonly TMockMongoContext DbContext;
+    protected readonly TMockMongoContext Context;
 
 
     protected BaseMongoTest(BaseMongoDatabaseFixture<TMockMongoContext> fixture)
     {
         Fixture = fixture;
-        DbContext = Fixture.GenerateDatabase();
-        // SeedAsync().Wait();
+        Context = Fixture.GenerateDatabase();
     }
 
 
